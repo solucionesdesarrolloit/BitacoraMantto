91cd55049a4993a364ce69a2df8d1c63f9e45899
clsoporte2 <clsoporte2@cgcsf.mx>
2026-03-17 16:06:37 -0600

    Fix: conexion cloud sql interna

--- bd.py
+++ bd.py
@@ -1,12 +1,12 @@
 import os
 from sqlalchemy import create_engine
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = "postgres"
+DB_PASS = "TU_PASSWORD"
+DB_NAME = "bitacoramantto"
+INSTANCE = "TU_PROYECTO:us-central1:bitacoramantto"
 
 engine = create_engine(
-    "postgresql://postgres:Pctr-3856.cpa.ppcolabs@34.173.142.84:5432/bitacoramantto"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}"
+    f"?host=/cloudsql/{"app-colaboradores-d408c:us-central1:postgres-bsf"}"
 )
