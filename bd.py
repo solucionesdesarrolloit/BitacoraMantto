b408db12df61c41ce3d495c54e0cf0b3c57f604b
clsoporte2 <clsoporte2@cgcsf.mx>
2026-03-17 16:12:05 -0600

    Conexion cloud sql

--- bd.py
+++ bd.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import create_engine
 
 DB_USER = "postgres"
-DB_PASS = "TU_PASSWORD"
+DB_PASS = "Pctr-3856.cpa.ppcolabs"
 DB_NAME = "bitacoramantto"
-INSTANCE = "TU_PROYECTO:us-central1:bitacoramantto"
+INSTANCE = "app-colaboradores-d408c:us-central1:postgres-bsf"
 
 engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}"
