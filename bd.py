f95b75fc2644ed7c5ad2bfc73c7524a029dd7052
clsoporte2 <clsoporte2@cgcsf.mx>
2026-03-18 10:15:07 -0600

    Modificacion de permisos

--- bd.py
+++ bd.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 
 DB_USER = "user_app"
-DB_PASS = "Pass2422$"
+DB_PASS = "Pass1234$"
 DB_NAME = "bitacoramantto"
 INSTANCE = "app-colaboradores-d408c:us-central1:postgres-bsf"
 
